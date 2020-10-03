df862495d96f9e16daa3b65ad1752a5221502c40
lsudupe <sudupelaura@gmail.com>
2020-10-03 17:04:27 +0200

    get a syntaxError in backspacesInString

--- codewars/backspacesInString.py
+++ codewars/backspacesInString.py
@@ -1,13 +1,27 @@
-s = 'ab#c###db'
+'''Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+Your task is to process a string with "#" symbols.'''
+
+string = 'ab#c###db'
+
+
+
+
+
+
+
+
+
 def backspaces(s):
-    position = []
-    for carac in s:
-        if s[carac] = '#':
-            position.append(s[carac])
-    return position
+    list = [chac for chac in enumerate(s)]
+    clean = []
+    for i,i2 in list:
+        if i2 == '#':
+            clean.append(list.remove(list[i2] and list[i2 - 1])
+print(clean)
+    final_string = ''.join(clean))
+    return final_string
 
-print(backspaces(s))
-
+print(backspaces(string))
 
 
 
